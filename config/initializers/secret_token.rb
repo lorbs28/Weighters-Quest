fb59ff57ca6553e24014cb2108105216d8f56431
Bryan Lor <lorbs28@gmail.com>
2011-09-28 17:55:38 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeightersQuest::Application.config.secret_token = '082ff839876a830fc3ba29cd1204056c3d58e6e05288b113987bac61935488a221a62ba3a97e74e2532dcceb34912f18d369fbfee82a5d675928d45c42522f70'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WeightersQuest::Application.config.session_store :cookie_store, key: '_WeightersQuest_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WeightersQuest::Application.config.session_store :active_record_store
